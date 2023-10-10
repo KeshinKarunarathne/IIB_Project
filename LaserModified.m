9ae1e5d7a32d1252e02b705512021d5dcbcbe942
Keshin Karunarathne <wksk2@cam.ac.uk>
2023-10-10 01:51:54 +0100

    Updated InitialOpticChannel.mls and supporting file

--- LaserModified.m
+++ LaserModified.m
@@ -1,27 +1,29 @@
-function [E] = LaserModified(Pcw, laserLinewidth, SpS, Rs, NSymb, NPol)
+function [Ereal, Ecomplex] = LaserModified(Pcw_dBm, centreLambda, lineWidth, SpS, Rs, NSymb, NPol)
 
-    % Calculating the linear power of the continuous wave:
-    PcwLinear = 1e-3*10^(Pcw/10);
-
+    c = 299792458; % Speed of light in m/s
+    PcwLinear = 10^(Pcw_dBm/10); % Linear power of laser output
+    T = 1/(SpS*Rs); % Period between samples
+    t= [0:1:NSymb*SpS - 1]*T; t=t'; % Salient time indices to sample E field
+    
+    % If the laser linewidth is not 0 Hz, phase noise is inserted 
+    Theta = zeros(size(t));
+    if lineWidth ~= 0
+        % Calculating the phase noise:
+        Var = 2*pi*lineWidth*T ;
+        Delta_theta = sqrt(Var)*randn(size(Theta,1),1);
+        Theta = cumsum(Delta_theta);
+    end
+    
     % Generating the electric field of the optical signal:
     if NPol == 1
-        E = ones(SpS*NSymb,1)*sqrt(PcwLinear);
+        Ereal = sqrt(PcwLinear)*sin(Theta + (2*pi*c/centreLambda)*t);
+        Ecomplex = sqrt(PcwLinear)*exp(1i*(2*pi*c/centreLambda)*t);
     elseif NPol == 2
-        E = ones(SpS*NSymb,2)*sqrt(PcwLinear/2);
+        Ereal(:,1) = sqrt(PcwLinear)*sin(Theta + (2*pi*c/centreLambda)*t);
+        Ereal(:,2) = Ereal(:,1);
+        Ecomplex(:,1) = sqrt(PcwLinear)*exp(1i*(2*pi*c/centreLambda)*t);
+        Ecomplex(:,2) = Ecomplex(:,1);
     else
         error('The possible number of polarizations used must be 1 or 2');
-    end
-
-    % If the laser linewidth is not 0 Hz, phase noise is inserted 
-    if laserLinewidth ~= 0
-        % Period between samples at the (oversampled) transmit. signal:
-        T = 1/(SpS*Rs);
-        % Calculating the phase noise:
-        Var = 2*pi*laserLinewidth*T ;
-        Delta_theta = sqrt(Var)*randn(size(E,1),1);
-        Theta = cumsum(Delta_theta);
-
-        % Adding phase noise to the optical signal:
-        E = E.*repmat(exp(1i*Theta),1,size(E,2));
     end
 end
